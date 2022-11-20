f0fd81f97296236d2505ba23db40456d767a70f5
sanjay kumar <70475208+snjy-kumar@users.noreply.github.com>
2022-11-20 20:10:00 +0000

    learning something

---
+++ .ccls-cache/@home@runner@Cbasics/array.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello therer only here I'M.\n");
+  return 0;
+}
---
+++ nmea.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Some thing learning on replit.\n");
+  return 0;
+}
