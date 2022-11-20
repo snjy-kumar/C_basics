867371a6e07547fcc134c146ef9060c382bb4903
sanjay kumar <70475208+snjy-kumar@users.noreply.github.com>
2022-11-20 19:57:52 +0000

    function

---
+++ .ccls-cache/@home@runner@Cbasics/fol@fuction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // to print your name, date of birth and mobile number
+  printf("Name    : Sanjay Kumar\n");
+  printf("DOB     : December 22, 2002\n");
+  printf("Mobile  : 91-8969892003\n");
+
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@Cbasics/function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  // 1. to print your name, date of birth and mobile number
+  // printf("Name    : Sanjay Kumar\n");
+  // printf("DOB     : December 22, 2002\n");
+  // printf("Mobile  : 91-8969892003\n");
+
+
+  printf("Hey There! I am using replit to write code and repository on my github profile and begin to learn codeing.\n");
+  return 0;
+}
